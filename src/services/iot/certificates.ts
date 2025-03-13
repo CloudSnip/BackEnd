6c103b1d609969442bc853b4d3c864bb4a174fe3
Fabio <fabio04musitelli@gmail.com>
2025-03-13 17:15:22 +0100

    ✨ iot connection and data retrieval

--- src/services/iot/certificates.ts
+++ src/services/iot/certificates.ts
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import config, { AwsConfig } from '../../config.ts';
 
 import fs from 'fs';
-import http from 'http';
+import https from 'https';
 import os from 'os';
 import { generalLogger } from '../logger/winston.ts';
 
@@ -10,34 +12,33 @@ const { secretMasterName, secretMasterUrl } = config.aws as AwsConfig;
 export function checkMasterCertificate() {
     return new Promise((resolve, reject) => {
         try {
-            const file = fs.readFileSync(`${os.tmpdir()}/${secretMasterName}.pem.crt`);
+            const file = fs.readFileSync(`${os.tmpdir()}}/${secretMasterName}.certificate.pem.crt`);
             resolve(file);
         } catch (err) {
+            generalLogger.error(err);
             generalLogger.info('Certificate not found, downloading...');
-            generalLogger.info(err);
 
             const finishedActions = [];
 
             const cert = fs.createWriteStream(`${os.tmpdir()}/${secretMasterName}.certificate.pem.crt`);
             const privateKey = fs.createWriteStream(`${os.tmpdir()}/${secretMasterName}.private.pem.key`);
-            const amazonRoot = fs.createWriteStream(`${os.tmpdir()}/AmazonRootCA1.pem`)
+            const amazonRoot = fs.createWriteStream(`${os.tmpdir()}/AmazonRootCA1.pem`);
 
-            http.get(`${secretMasterUrl}/${secretMasterName}.certificate.pem.crt`,
-                function (response: { pipe: (arg0: any) => void; }) {
-                    response.pipe(cert);
-                });
+            generalLogger.info(`${secretMasterUrl}/${secretMasterName}.certificate.pem.crt`);
+            https.get(`${secretMasterUrl}/${secretMasterName}.certificate.pem.crt`, function (response: { pipe: (arg0: any) => void; }) {
+                response.pipe(cert);
+            });
 
-            http.get(`${secretMasterUrl}/${secretMasterName}.private.pem.key`,
-                function (response: { pipe: (arg0: any) => void; }) {
-                    response.pipe(privateKey);
-                });
+            https.get(`${secretMasterUrl}/${secretMasterName}.private.pem.key`, function (response: { pipe: (arg0: any) => void; }) {
+                response.pipe(privateKey);
+            });
 
-            http.get(`${secretMasterUrl}/AmazonRootCA1.pem`,
-                function (response: { pipe: (arg0: any) => void; }) {
-                    response.pipe(amazonRoot);
-                });
+            https.get(`${secretMasterUrl}/AmazonRootCA1.pem`, function (response: { pipe: (arg0: any) => void; }) {
+                response.pipe(amazonRoot);
+            });
 
             cert.on('finish', function () {
+                generalLogger.info('Certificates downloaded');
                 finishedActions.push(true)
 
                 if (finishedActions.length === 3) {
@@ -50,6 +51,7 @@ export function checkMasterCertificate() {
             });
 
             amazonRoot.on('finish', function () {
+                generalLogger.info('Certificates downloaded');
                 finishedActions.push(true)
 
                 if (finishedActions.length === 3) {
@@ -62,6 +64,7 @@ export function checkMasterCertificate() {
             });
 
             privateKey.on('finish', function () {
+                generalLogger.info('Certificates downloaded');
                 finishedActions.push(true)
 
                 if (finishedActions.length === 3) {
