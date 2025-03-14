d6073ecf89dda18b012c65cfdaaa9cd0733254f3
Fabio <fabio04musitelli@gmail.com>
2025-03-14 19:56:27 +0100

    removed appwrite service

--- src/api/auth/controller.ts
+++ src/api/auth/controller.ts
@@ -1,14 +1,8 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 import passport from 'passport';
-import User, { IUser, SocialProvider } from '../users/model.ts'
+import User, { IUser } from '../users/model.ts'
 import { UsersRoleEnum } from '../../utils/enum.ts';
 import { generateToken } from '../../services/auth/jwt.ts';
-import client, { account } from '../../services/appwrite/index.ts';
-import { OAuthProvider, Query, Users } from 'node-appwrite';
-import _ from 'lodash';
-import Config from '../../config.ts';
-
-const users = new Users(client);
 
 export const register: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -57,82 +51,4 @@ export const login = (req: Request, res: Response, next: NextFunction): void => 
             }
         }
     )(req, res, next);
-};
-
-export const authGoogle = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const baseUrl = `${req.protocol}://${req.get('host')}`;
-        if (!baseUrl) {
-            throw new Error('Base URL not found');
-        }
-        const redirectURL = await account.createOAuth2Token(OAuthProvider.Google,
-            `${baseUrl}/auth/google/success`)
-        res.send(redirectURL);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
-
-export const authSuccess = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { userId, secret } = req.query as { userId: string; secret: string };
-        await account.createSession(userId, secret);
-        const user = await users.get(userId);
-
-        const identity = await users.listIdentities([Query.equal('userId', userId), Query.equal('provider', OAuthProvider.Google)]);
-        if (!identity || identity.total === 0) {
-            throw new Error('No Google identity found');
-        }
-        const { email, name } = user;
-        const [firstName, ...lastNameParts] = name.split(' ');
-        const lastName = lastNameParts.join(' ');
-        const username = email.split('@')[0];
-
-        const socialProvider = _.find(identity.identities, (identity) => {
-            return identity.provider === OAuthProvider.Google;
-        });
-        if (!socialProvider) {
-            throw new Error('No Google identity found');
-        }
-
-        const socialProviders: SocialProvider[] = [
-            {
-                providerName: OAuthProvider.Google,
-                identityId: socialProvider.$id,
-            },
-        ];
-
-        const userData: Partial<IUser> = {
-            username,
-            email,
-            socialProviders,
-            firstName: firstName || '',
-            lastName: lastName || '',
-            role: UsersRoleEnum.USER,
-        };
-        let existingUser = await User.findOne({
-            email,
-            socialProviders: {
-                $elemMatch: {
-                    providerName: OAuthProvider.Google,
-                    identityId: socialProvider.$id,
-                },
-            },
-        });
-
-        if (!existingUser) {
-            existingUser = new User(userData);
-            await existingUser.save();
-
-            if (!existingUser) {
-                throw new Error('There was an error creating the user');
-            }
-        }
-
-        const token = generateToken(existingUser);
-        res.redirect(Config.clientUrl + '/auth?token=' + token + '&user=' + JSON.stringify(existingUser));
-
-    } catch (error) {
-        res.status(500).json({ ERROR: (error as Error).message });
-    }
 };
--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { authGoogle, authSuccess, login, register } from './controller.ts';
+import { login, register } from './controller.ts';
 import { loginSchema, registerSchema } from './middlewares/index.ts';
 import { validateBody } from '../../services/validator/body/index.ts';
 
@@ -60,29 +60,5 @@ router.post('/register', validateBody(registerSchema), register);
  *         description: Unauthorized
  */
 router.post('/login', validateBody(loginSchema), login);
-
-/**
- * @swagger
- * /auth/google:
- *   get:
- *     tags: [Auth]
- *     summary: Authenticate with Google
- *     responses:
- *       302:
- *         description: Redirects to Google for authentication.
- */
-router.get('/google', authGoogle);
-
-/**
- * @swagger
- * /auth/google/success:
- *   get:
- *     tags: [Auth]
- *     summary: Google Authentication Success
- *     responses:
- *       200:
- *         description: Successfully authenticated with Google.
- */
-router.get('/google/success', authSuccess);
 
 export default router;
